0b41c07543e2659cdd92ec5f9347976dee9ef40e
varsha3004 <varshahs30@gmail.com>
2021-02-19 20:15:05 +0530

    deleted

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources.item import Item, ItemList
 from resources.store import Store,StoreList
 
 app= Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('HEROKU_POSTGRESQL_AQUA_URL','sqlite3:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('HEROKU_POSTGRESQL_AQUA_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['PROPAGATE_EXCEPTIONS']=True
 app.secret_key= 'jose'
